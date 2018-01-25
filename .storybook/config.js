9e44a0e2242bbbdbff20337c1957d3b68cc929a9
Rob Caldecott <robert.caldecott@gmail.com>
2018-01-25 19:57:09 +0000

    Added MDC theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import "typeface-roboto";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import "material-components-web/dist/material-components-web.css";
 import { configure, addDecorator } from "@storybook/react";
+import "./theme.css";
 
 const loadStories = () => {
   require("glob-loader!./stories.pattern");
